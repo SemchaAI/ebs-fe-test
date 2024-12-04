9450c2c58e913bd834605450cf35f3384b28ec72
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-12-04 09:55:19 +0200

    feact: one enum for all api routes

---
+++ src/services/api/routes.ts
@@ -0,0 +1,7 @@
+const BASE_API_URL = 'https://fakestoreapi.com';
+
+export enum ApiRoutes {
+  PRODUCTS = `${BASE_API_URL}/products`,
+  CATEGORIES = `${BASE_API_URL}/products/categories`,
+  CATEGORY_PRODUCTS = `${BASE_API_URL}/products/category`,
+}
