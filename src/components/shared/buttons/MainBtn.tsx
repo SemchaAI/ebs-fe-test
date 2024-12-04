97dc594f14c0ff0ad9162c6fd88d2e587dfbd56f
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-12-04 21:44:42 +0200

    feat(btn): new custom btn with own styles

---
+++ src/components/shared/buttons/MainBtn.tsx
@@ -0,0 +1,35 @@
+import type { IBtn } from '@/models/buttons';
+import css from './mainBtn.module.scss';
+
+interface IBtnProps extends IBtn {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const MainBtn = function ({
+  className = '',
+  children,
+  version = 'text',
+  size = 'default',
+  icon = false,
+  type = 'button',
+  disabled = false,
+  label = 'button',
+  onClick,
+  ...props
+}: IBtnProps) {
+  const btnClass = `${css.button} ${css[version]} ${icon ? css.icon : ''} 
+  ${css[size]} ${className}`;
+  return (
+    <button
+      type={type}
+      className={btnClass}
+      disabled={disabled}
+      onClick={onClick}
+      aria-label={label}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
