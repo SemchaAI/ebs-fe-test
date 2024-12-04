3864e0f78170aca4238f691e4847f50610e8139b
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-12-04 21:40:53 +0200

    chore(config): update for testing libs

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,10 @@ export default defineConfig(({ mode }) => {
       },
     },
     plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+    },
   };
 });
