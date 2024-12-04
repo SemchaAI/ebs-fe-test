5725d69fcc5202fd232bf93b8c458a2b4e67d7c2
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-12-04 10:21:06 +0200

    feat: basic header with logo and navigation in future

---
+++ src/components/widgets/headers/MainHeader.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { ShoppingCart, Squirrel } from 'lucide-react';
+import { Container } from '@/components/shared';
+import css from './mainHeader.module.scss';
+
+export const MainHeader = () => {
+  return (
+    <header className={css.header}>
+      <Container>
+        <div className={css.headerContainer}>
+          <Link
+            className={css.logoLink}
+            to="/"
+            title="Squirrel shop main page"
+          >
+            <Squirrel
+              width={45}
+              height={45}
+              className={css.logoIcon}
+            />
+            <div className={css.logoTextBlock}>
+              <h1 className={css.logoTitle}>
+                <span>Squirrel</span>Shop
+              </h1>
+              <p className={css.logoText}>
+                <span>Fastest</span> <span>delivery</span>
+              </p>
+            </div>
+          </Link>
+          <ShoppingCart />
+        </div>
+      </Container>
+    </header>
+  );
+};
