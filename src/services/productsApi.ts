6955a2711969256a11bf26801efacda23895bbe9
Semciuc Nichita <nikitasemciuc@gmail.com>
2024-12-05 12:00:53 +0200

    fix(update): one api endpoint for products
    I was confused by the API, which has no filtering, sorting, and pagination options (as query params ?page=1&limit=4&sort=desc&category=jewelry)

--- src/services/productsApi.ts
+++ src/services/productsApi.ts
@@ -1,18 +1,31 @@
 import { IProduct } from '@/models/product';
 import { ApiRoutes } from './api/routes';
 
-export const getProducts = async (): Promise<IProduct[]> => {
-  const response = await fetch(`${ApiRoutes.PRODUCTS}`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch products');
-  }
-  const data: IProduct[] = await response.json();
-  return data;
-};
-export const getCategoryProducts = async (
+// export const getProducts = async (): Promise<IProduct[]> => {
+//   const response = await fetch(`${ApiRoutes.PRODUCTS}`);
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch products');
+//   }
+//   const data: IProduct[] = await response.json();
+//   return data;
+// };
+// export const getCategoryProducts = async (
+//   category: string
+// ): Promise<IProduct[]> => {
+//   const response = await fetch(`${ApiRoutes.CATEGORY_PRODUCTS}/${category}`);
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch category products');
+//   }
+//   const data: IProduct[] = await response.json();
+//   return data;
+// };
+export const getSmartProducts = async (
   category: string
+  //limit = 4
 ): Promise<IProduct[]> => {
-  const response = await fetch(`${ApiRoutes.CATEGORY_PRODUCTS}/${category}`);
+  const currentCategory = category !== '' ? `category/${category}` : '';
+  //?limit=${limit}
+  const response = await fetch(`${ApiRoutes.PRODUCTS}/${currentCategory}`);
   if (!response.ok) {
     throw new Error('Failed to fetch category products');
   }
